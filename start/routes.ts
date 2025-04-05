72b443fe95b07bcef176d3a19e1ebf2d838d6c60
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-04-05 16:27:23 +0400

    Tested route creation and stateful information passing to the views

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,8 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/movies', async (ctx) => {
+  ctx.view.share({ movie: 'My awesome movie' })
+  return ctx.view.render('pages/movies')
+})
