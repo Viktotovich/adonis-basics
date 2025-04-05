44a18bffd1bd203a3f478b69cc92ae9c9c1ac149
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-04-05 16:44:35 +0400

    Tested rendering through param ids / slugs. <%=%> equivalent of EJS

--- start/routes.ts
+++ start/routes.ts
@@ -7,11 +7,16 @@
 |
 */
 
+import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
+import fs from 'node:fs/promises'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
 
-router.get('/movies', async (ctx) => {
-  ctx.view.share({ movie: 'My awesome movie' })
-  return ctx.view.render('pages/movies')
-})
+router
+  .get('/movies/:slug', async (ctx) => {
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+    const movie = await fs.readFile(url, 'utf-8')
+    return ctx.view.render('pages/movies', { movie })
+  })
+  .as('movies.show')
