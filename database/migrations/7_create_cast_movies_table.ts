35a5aae7de8fc902f739eb28efae8958b80afb3f
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-04-30 18:26:30 +0400

    Major breaking bug fixed while working on many to many relationships in factories:
    .integer was cineasts vs cineast (singular form) which messed up the seeders as the pivot column name didnt match

--- database/migrations/7_create_cast_movies_table.ts
+++ database/migrations/7_create_cast_movies_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
       table
-        .integer('cineasts_id')
+        .integer('cineast_id')
         .unsigned()
         .references('cineasts.id')
         .notNullable()
--- database/seeders/01_fake_seeder.ts
+++ database/seeders/01_fake_seeder.ts
@@ -38,7 +38,17 @@ export default class extends BaseSeeder {
       index++
     }).createMany(movies.length)
 
-    await MovieFactory.with('director').with('writer').createMany(3)
+    await MovieFactory.with('director')
+      .with('writer')
+      .with('castMembers', 3, (builder) =>
+        builder.pivotAttributes([
+          { character_name: 'Robert', sort_order: 0 },
+          { character_name: 'Joy', sort_order: 1 },
+          { character_name: 'Anna', sort_order: 2 },
+        ])
+      )
+      .with('crewMembers', 5, (builder) => builder.pivotAttributes({ title: 'Camera Operator' }))
+      .createMany(3)
     await MovieFactory.apply('released').with('director').with('writer').createMany(2)
     await MovieFactory.apply('releasingSoon').with('director').with('writer').createMany(2)
     await MovieFactory.apply('postProduction').with('director').with('writer').createMany(2)
