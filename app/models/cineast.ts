aaccbe62eb1a0a3fb52e1517503069e8de6042ef
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-04-30 17:19:15 +0400

    Worked with defining many to many relationships

--- app/models/cineast.ts
+++ app/models/cineast.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, computed, hasMany } from '@adonisjs/lucid/orm'
+import { BaseModel, column, computed, hasMany, manyToMany } from '@adonisjs/lucid/orm'
 import Movie from './movie.js'
-import type { HasMany } from '@adonisjs/lucid/types/relations'
+import type { HasMany, ManyToMany } from '@adonisjs/lucid/types/relations'
 
 export default class Cineast extends BaseModel {
   @column({ isPrimary: true })
@@ -36,4 +36,18 @@ export default class Cineast extends BaseModel {
     foreignKey: 'writerId',
   })
   declare moviesWritten: HasMany<typeof Movie>
+
+  @manyToMany(() => Movie, {
+    pivotTable: 'crew_movies',
+    pivotTimestamps: true,
+  })
+  declare crewMovies: ManyToMany<typeof Movie>
+
+  @manyToMany(() => Movie, {
+    pivotTable: 'cast_movies',
+    pivotTimestamps: true,
+    //eager load >> Adonis provides it when this relation is loaded
+    pivotColumns: ['character_name', 'sort_order'],
+  })
+  declare castMovies: ManyToMany<typeof Movie>
 }
--- app/models/movie.ts
+++ app/models/movie.ts
@@ -1,9 +1,9 @@
 import MovieStatuses from '#enums/movie_statuses'
-import { BaseModel, beforeCreate, belongsTo, column, scope } from '@adonisjs/lucid/orm'
+import { BaseModel, beforeCreate, belongsTo, column, manyToMany, scope } from '@adonisjs/lucid/orm'
 import { DateTime } from 'luxon'
 import string from '@adonisjs/core/helpers/string'
 import MovieStatus from './movie_status.js'
-import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import type { BelongsTo, ManyToMany } from '@adonisjs/lucid/types/relations'
 import Cineast from './cineast.js'
 
 export default class Movie extends BaseModel {
@@ -57,6 +57,28 @@ export default class Movie extends BaseModel {
     foreignKey: 'writerId',
   })
   declare writer: BelongsTo<typeof Cineast>
+
+  /*verbose options / didn't include as it is the defaults
+  {
+    localKey: 'id',
+    relatedKey: 'id',
+    pivotForeignKey: 'movie_id',
+    pivotRelatedForeignKey: 'cineast_id',
+  }
+   */
+  @manyToMany(() => Cineast, {
+    pivotTable: 'crew_movies',
+    pivotTimestamps: true,
+  })
+  declare crewMembers: ManyToMany<typeof Cineast>
+
+  @manyToMany(() => Cineast, {
+    pivotTable: 'cast_movies',
+    pivotTimestamps: true,
+    //eager load >> Adonis provides it when this relation is loaded
+    pivotColumns: ['character_name', 'sort_order'],
+  })
+  declare castMembers: ManyToMany<typeof Cineast>
 
   static released = scope((query) => {
     query.where((group) =>
